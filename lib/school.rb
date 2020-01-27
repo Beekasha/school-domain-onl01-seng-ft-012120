4ee3e22043421b5b66bf01ece4cb5a9193b523c5
Brian Kleiner <brian.j.kleiner@gmail.com>
2020-01-27 22:36:36 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+
+    attr_accessor :roster
+    attr_reader :school_name
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+
+    def add_student(student_name, grade)
+        if @roster[grade] == nil
+            @roster[grade] = []
+            @roster[grade] << student_name
+        else
+            @roster[grade] << student_name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        
+       @roster = @roster.sort.to_h
+        @roster.sort_by do |grade, names|
+            names = names.sort_by! do |name| #! modifies the array of names itself
+                name.downcase
+            end
+        end
+        @roster
+    end
+end
